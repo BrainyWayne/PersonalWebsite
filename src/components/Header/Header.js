e1c82f27c7e8e94e6ba25268af57d2567d2d25bd
Francis Eshun <eshunfrancisboateng@gmail.com>
2022-05-03 21:04:37 +0000

    Fixed ico

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -28,7 +28,7 @@ const Header = () =>  (
       </li>
       <li>
         <Link href="#about">
-          <NavLink>About</NavLink>
+          <NavLink>About Me</NavLink>
         </Link>
       </li>
     </Div2>
